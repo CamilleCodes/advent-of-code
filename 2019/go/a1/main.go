0a1bc1bbdea00d011be016e91d1a316c1ac06cd5
Camille Rosewright <camille.rosewright@gmail.com>
2023-08-12 17:20:36 +0900

    feat: complete part one of day one for 2019

--- 2019/go/a1/main.go
+++ 2019/go/a1/main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// Part 1
+
+// getFile opens the input file and returns a pointer to the file
+func getFile(path string) *os.File {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file")
+		log.Fatal(err)
+	}
+
+	return file
+}
+
+// getModuleMass scans the input file and returns the mass of the
+// module mass listed in the input file
+func getModuleMass(input string) int {
+	mass, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Error converting string to int")
+		log.Fatal(err)
+	}
+
+	return mass
+}
+
+// getFuelRequired takes a mass and returns the amount of fuel required
+// for a module of that mass
+func getFuelRequired(mass int) int {
+	return mass/3 - 2
+}
+
+func main() {
+	file := getFile("a1/input.txt")
+	defer file.Close()
+
+	var fuelTotal = 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		mass := getModuleMass(scanner.Text())
+		fuel := getFuelRequired(mass)
+		fuelTotal += fuel
+	}
+
+	fmt.Println(fuelTotal)
+}
