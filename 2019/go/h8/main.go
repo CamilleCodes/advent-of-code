8ab8458cf6e1685328443f526a4f8ec15b288a82
Camille Rosewright <camille.rosewright@gmail.com>
2023-09-26 10:23:43 +0900

    feat: complete part two of day eight for 2019

--- 2019/go/h8/main.go
+++ 2019/go/h8/main.go
@@ -55,13 +55,68 @@ func main() {
 	lowestLayer := 0
 	lowest := zeros[0]
 	for layer := range layers {
-		// fmt.Println("layer", layer, "has", zeros[layer], "zeros")
 		if zeros[layer] < lowest {
 			lowestLayer = layer
 			lowest = zeros[layer]
 		}
 	}
 
-	fmt.Println("lowest zeros are in layer", lowestLayer)
-	fmt.Println("1s*2s:", ones[lowestLayer]*twos[lowestLayer])
+	fmt.Println(
+		"layer", lowestLayer,
+		"has", lowest, "zeros,",
+		ones[lowestLayer], "ones,",
+		"and", twos[lowestLayer], "twos",
+	)
+	fmt.Println("Part 1 (1s*2s):", ones[lowestLayer]*twos[lowestLayer])
+
+	decodedImage := make([]string, imagePixelSize)
+	for i := 0; i < imagePixelSize; i++ {
+		for _, layer := range layers {
+			if layer[i] != "2" {
+				decodedImage[i] = layer[i]
+				break
+			}
+		}
+	}
+
+	for pixel := range decodedImage {
+		if decodedImage[pixel] == "0" {
+			decodedImage[pixel] = " "
+		}
+	}
+
+	fmt.Println("Part 2 (decoded image):")
+	for i := 0; i < imagePixelSize; i += width {
+		fmt.Println(strings.Join(decodedImage[i:i+width], ""))
+	}
 }
+
+// Part 2
+// 0222112222120000
+
+// Layers with their pixels
+// Layer 1: 02
+//          22
+
+// Layer 2: 11
+//          22
+
+// Layer 3: 22
+//          12
+
+// Layer 4: 00
+//          00
+
+// 0 - black
+// 1 - white
+// 2 - transparent
+
+// Check layer one, pixel by pixel
+// 1. If pixel is 0 or 1, then we're done (capture it in the final image)
+// 2. If pixel is 2, check the next layer
+// 	2a. In the next layer, return to step 1
+// 3. If there are no more layers to check, then the pixel is transparent
+
+// Decoded image
+// 01
+// 10
